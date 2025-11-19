bcd626da4ec874aa213847df96f0107280eeeb43
ethanTouitou <150148202+ethanTouitou@users.noreply.github.com>
2025-11-19 15:17:56 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import node from '@astrojs/node';         // ← ajoute l'adaptateur Node
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',                       // ← important pour avoir dist/server/
+  adapter: node({
+    mode: 'standalone'                    // ← recommandé (serveur Node autonome)
+  }),
   vite: {
     plugins: [tailwindcss()]
   }
